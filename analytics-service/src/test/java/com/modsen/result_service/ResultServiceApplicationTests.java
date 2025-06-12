bfefc28fb9ab7b8d9290e361151dd08100bc80d4
velshletter <velshletter@gmail.com>
2025-06-12 14:12:54 +0300

    Project structure

---
+++ analytics-service/src/test/java/com/modsen/result_service/ResultServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modsen.result_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-service/src/test/java/com/modsen/user_service/UserrrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modsen.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserrrServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
